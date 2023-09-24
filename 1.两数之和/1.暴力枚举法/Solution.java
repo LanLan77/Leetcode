99f88c2f42e6596495c1e4992c19d92cf6eb33f9
LanLan77 <775172380@qq.com>
2023-09-24 15:50:23 +0930

    1.两数之和--暴力枚举法

---
+++ 1.两数之和/1.暴力枚举法/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+// 暴力枚举法
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i< nums.length; i++){
+            for(int j = 0; j < nums.length;j++){
+                if(nums[i]+nums[j]==target && i != j){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return null;
+    }
+
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int[] arr = {2,7,11,15};
+        int target = 9;
+        int[] result = solution.twoSum(arr, target);
+        System.out.print(Arrays.toString(result));
+    }
+}
